74470db2b0f0745d556dc9e209183be78623e9a3
jzolo22 <jzolotarev@gmail.com>
2020-11-17 18:21:00 -0500

    done for now

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    const taskForm = document.querySelector("#create-task-form")
+
+    taskForm.addEventListener("submit", function(event){
+      event.preventDefault()
+      const newTask = event.target.querySelector("#new-task-description").value + "  "
+
+      const list = document.querySelector("#tasks")
+      const task = document.createElement('li')
+      console.log(task)
+      console.log(list)
+      task.textContent = newTask
+      list.append(task)
+      
+      const deleteButton = document.createElement("button")
+      deleteButton.textContent = "X"
+      deleteButton.style.color = "red"
+
+
+      task.append(deleteButton)
+
+      deleteButton.addEventListener("click", function(event){
+        task.remove()
+      })
+
+
+      event.target.reset()
+    })
+    
+   
 });
+
+
+// step 1: figure out whch element should be acted upon
+// step 2: what kind of event are we listening for 
+// step 3: figure out what the action should be 
+
+// step 1: find the parent element where the new one should live
+// step 2: create the new element & give it correct content
+// step 3: slap it on the DOM
